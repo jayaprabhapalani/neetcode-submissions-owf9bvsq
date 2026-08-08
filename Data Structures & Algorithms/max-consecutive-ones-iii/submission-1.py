4b77ddf9d7e63efa6448d77b9a306767b5491187
Jayaprabha <146448216+jayaprabhapalani@users.noreply.github.com>
2026-08-08 21:40:55 +0530

    Add: max-consecutive-ones-iii - submission-1

---
+++ Data Structures & Algorithms/max-consecutive-ones-iii/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        
+        max_l=0
+        left=0
+        z_cnt=0
+        
+
+        for right in range(len(nums)):
+            
+            if nums[right]==0:
+                    z_cnt+=1
+
+            # shrinking the left pointer until it reaches the 
+            #appropriate ptr to start the calculation again
+            #so we can reduce the cost
+            while(z_cnt>k):
+                if(nums[left]==0):
+                    z_cnt-=1
+                left+=1
+
+            max_l=max(max_l,right-left+1)
+
+            
+        return max_l
+        
